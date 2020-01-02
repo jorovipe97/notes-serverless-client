d46112aa133573afa637e0d8f20705977154190f
romualdo97 <jorovipe@gmail.com>
2020-01-01 20:45:46 -0500

    Configured aws amplify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+import Routes from "./Routes";
+// Collapse menu on react using boostrap: https://www.bennettnotes.com/bootstrap-navbar-collapse-reactjs/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">Scratch</Link>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <Link className="nav-link" to="/signup">Signup</Link>
+            </li>
+            <li class="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes />
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-upload-jorovipe"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://dg28tziy2h.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_WwKpeSjs2",
+        APP_CLIENT_ID: "4kceqhv1ec4ihedjo5261frg52",
+        IDENTITY_POOL_ID: "us-east-2:1a23f59b-e32b-4a7e-ab5e-245b3d00c7c4"
+    }
+};
